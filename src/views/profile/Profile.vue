c58f1ae5cb146daf4f4df4c885eec7c21c173637
fglytest <fglytest01@163.com>
2021-06-09 18:54:24 +0800

    '大致框架结构完成'

---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <scroll class="content">
+      <ul>
+        <li>个人信息1</li>
+      </ul>
+    </scroll>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Profile"
+}
+</script>
+
+<style scoped>
+
+</style>
